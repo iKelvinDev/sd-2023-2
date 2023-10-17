744a0c8655cfd65edd53b74bb701effd2f6963f9
Kelvin Marques <115046013+iKelvinDev@users.noreply.github.com>
2023-10-17 14:38:19 +0000

    add lib calculadora

---
+++ elixir/calculadora/lib/calculadora.ex
@@ -0,0 +1,19 @@
+defmodule Calculadora do
+
+  def soma(a, b) when is_number(a) and is_number(b) do
+    a + b
+  end
+
+  def subtracao(a, b) when is_number(a) and is_number(b) do
+    a - b
+  end
+
+  def multiplicacao(a, b) when is_number(a) and is_number(b) do
+    a * b
+  end
+
+  def divisao(_, 0), do: raise "Divisão por zero."
+  def divisao(a, b) when is_number(a) and is_number(b) do
+    a / b
+  end
+end
