f9c6ab87fcf21ec6ff892cef41ffdc4862649b2a
Kelvin Marques <115046013+iKelvinDev@users.noreply.github.com>
2023-10-24 08:21:34 +0000

    Implementado cálculo da equação do 2 grau

--- elixir/equacao2grau/lib/equacao2grau.ex
+++ elixir/equacao2grau/lib/equacao2grau.ex
@@ -1,3 +1,21 @@
 defmodule Equacao2Grau do
+    import :math
+
+    def calcular(a, b, c) when a != 0 do
+    delta = :math.pow(b, 2) - 4 * a * c
+
+    if delta < 0 do
+      "Equação não possui raízes reais."
+    else
+      x1 = (-b + :math.sqrt(delta)) / (2 * a)
+      x2 = (-b - :math.sqrt(delta)) / (2 * a)
+
+      if delta == 0 do
+        "Equação possui uma raiz real: x = #{x1}"
+      else
+        "A equação possui duas raízes reais: x1 = #{x1} e x2 = #{x2}"
+      end
+    end
+  end
 
 end
